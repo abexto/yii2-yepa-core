5da064db8cfae72f515d945695eb05ec9c5cc306
andreas <andreas@hc-devel-xu12>
2015-10-31 18:05:06 +0100

    Initial

---
+++ lib/helpers/Rules.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2015 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\yepa\core\helpers;
+
+/**
+ * Description of Rules
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class Rules
+{
+    //put your code here
+}
